44e3690290863fcfba7427e82f217694ad71107b
Fellipe Cechinel <fellipecechinel@gmail.com>
2020-06-14 00:31:56 -0300

    Changed models Category and Transaction to use typeorm

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,51 @@
+import {
+  Column,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  ValueTransformer,
+} from 'typeorm';
+import Category from './Category';
+
+const columnNumericTransformer: ValueTransformer = {
+  from: (data?: string | null) => {
+    return data ? parseFloat(data) : null;
+  },
+  to: (data?: number | null) => {
+    return data || null;
+  },
+};
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('numeric', {
+    transformer: columnNumericTransformer,
+  })
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
